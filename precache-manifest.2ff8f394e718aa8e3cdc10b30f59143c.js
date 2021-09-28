b045acc5c7e9d576b8743c3dd821d754823a7616
Code Sandbox <hello@codesandbox.io>
2021-09-28 17:00:55 +0000

    Deployment from Codesandbox.

---
+++ precache-manifest.2ff8f394e718aa8e3cdc10b30f59143c.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "026b99415949a4a8afde7682782964e6",
+    "url": "https://harshtalks.github.io/csb-h00kg/index.html"
+  },
+  {
+    "revision": "6bd28464f2d807af11a6",
+    "url": "https://harshtalks.github.io/csb-h00kg/static/css/main.03a90927.chunk.css"
+  },
+  {
+    "revision": "fe204de9cdd93e05221e",
+    "url": "https://harshtalks.github.io/csb-h00kg/static/js/2.8a6ff14f.chunk.js"
+  },
+  {
+    "revision": "135edfc18d9eaf91135ab0022c7da8fb",
+    "url": "https://harshtalks.github.io/csb-h00kg/static/js/2.8a6ff14f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6bd28464f2d807af11a6",
+    "url": "https://harshtalks.github.io/csb-h00kg/static/js/main.3bf0f3be.chunk.js"
+  },
+  {
+    "revision": "0ef1b25a70bbd746c5bf",
+    "url": "https://harshtalks.github.io/csb-h00kg/static/js/runtime-main.2db92e91.js"
+  },
+  {
+    "revision": "f9a44e1ccb9f44cd4de1da8fe7f6f4d8",
+    "url": "https://harshtalks.github.io/csb-h00kg/static/media/arrow-left.f9a44e1c.svg"
+  },
+  {
+    "revision": "dc372a07777e87891aaac087cfbe60ad",
+    "url": "https://harshtalks.github.io/csb-h00kg/static/media/arrow-right.dc372a07.svg"
+  },
+  {
+    "revision": "e84c205576ce51cc6a90653c49df11f0",
+    "url": "https://harshtalks.github.io/csb-h00kg/static/media/curology-min.e84c2055.png"
+  },
+  {
+    "revision": "379bad944ff3f73142989e87c5b24773",
+    "url": "https://harshtalks.github.io/csb-h00kg/static/media/lumin-min.379bad94.png"
+  },
+  {
+    "revision": "24e8d8f29296438b099443772c822a1f",
+    "url": "https://harshtalks.github.io/csb-h00kg/static/media/up-arrow-cirlce.24e8d8f2.svg"
+  },
+  {
+    "revision": "5f9e30771ee4dbbad1689af16cb49028",
+    "url": "https://harshtalks.github.io/csb-h00kg/static/media/yourspace-min.5f9e3077.png"
+  }
+]);
